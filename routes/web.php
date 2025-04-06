0e3c9e5b006573baf0f9eecdb923e6e038792d66
Jane Etti <etti.jane@gmail.com>
2025-04-06 21:02:58 +0300

    Add Forms and CSRF

--- routes/web.php
+++ routes/web.php
@@ -3,26 +3,38 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
-
-
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->simplePaginate(3);
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
-    return view('jobs', [
-        'jobs' => $jobs     
+    return view('jobs.index', [
+        'jobs' => $jobs
     ]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
-    //dd($job);
+    return view('jobs.show', ['job' => $job]);
+});
 
-    return view('job', ['job' => $job]);
+Route::post('/jobs', function () {
+    // validation...
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
